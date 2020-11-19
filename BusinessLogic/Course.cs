2af9f68acb0cddee11e401acf0c2c83c79434560
SethSterling <sethbdsterling@gmail.com>
2020-11-18 18:05:31 -0800

    Modified Course Class so that it passes unit  tests

--- BusinessLogic/Course.cs
+++ BusinessLogic/Course.cs
@@ -6,9 +6,13 @@ namespace BusinessLogic
 {
     public class Course
     {
+        private byte numberOfCredits;
+        private string courseName;
+
         public Course(string courseName)
         {
             CourseName = courseName;
+            InstructorName = "STAFF";
         }
 
         /// <summary>
@@ -19,12 +23,40 @@ namespace BusinessLogic
         /// <summary>
         /// The name of the course
         /// </summary>
-        public string CourseName { get; set; }
+        public string CourseName 
+        { 
+            get => courseName;
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException();
+                }
+                else
+                {
+                    courseName = value;
+                }
+            }
+        }
 
         /// <summary>
         /// Number of credits for the course
         /// </summary>
-        public byte NumberOfCredits { get; set; }
+        public byte NumberOfCredits
+        {
+            get => numberOfCredits;
+            set
+            {
+                if (value <= 0 || value > 29)
+                {
+                    throw new ArgumentException();
+                }
+                else
+                {
+                    numberOfCredits = value;
+                }
+            }
+        }
 
     }
 }
